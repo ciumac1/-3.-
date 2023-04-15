b6f1318862040f1e03687f3bfbec1139075c7ef7
ciumac <ciumaccristian@gmail.com>
2023-04-15 04:48:41 +0300

    пытаюсь понять что делаю)

---
+++ homeworklesson1.01/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+
+// Ввод чисел от пользователя
+Console.Write("Введите первое число: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+double num3 = Convert.ToDouble(Console.ReadLine());
+
+// Нахождение максимального числа
+double maxNum = Math.Max(num1, Math.Max(num2, num3));
+
+// Вывод результата
+Console.WriteLine("Максимальное число: " + maxNum);
---
+++ homeworklesson1.02/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+
+// Ввод чисел от пользователя
+Console.Write("Введите первое число: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+// Сравнение чисел
+if (num1 > num2)
+    {
+        Console.WriteLine($"{num1} больше {num2}");
+            Console.WriteLine($"{num2} меньше {num1}");
+        }
+else if (num1 < num2)
+        {
+            Console.WriteLine($"{num2} больше {num1}");
+            Console.WriteLine($"{num1} меньше {num2}");
+            }
+            
+else {
+            Console.WriteLine($"{num1} и {num2} равны");
+        }
